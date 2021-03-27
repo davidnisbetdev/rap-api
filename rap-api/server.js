17506fb5a3bebbcf0536dcf1821f564a1383431f
David <nizz3hx@protonmail.com>
2021-03-27 15:06:28 +0000

    To push to Heroku

---
+++ rap-api/server.js
@@ -0,0 +1,44 @@
+const { response } = require('express')
+const express =require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+
+let rappers = {
+    '21 Savage': {
+    'age': 28,
+    'birthName': 'Sheyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England'
+    },
+    'Chance The Rapper': {
+    'age': 27,
+    'birthName': 'Chancelor Jonathan Bennet',
+    'birthLocation': 'Chicaho, Illinois'
+    },
+    'dylan': {
+    'age': '28',
+    'birthName': 'dylan',
+    'birthLocation': 'dylan' 
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/rappers/:rapperName', (req, res) => {
+    const rapName = req.params.rapperName.toLowerCase()
+    console.log(rapName)
+    if(rappers[rapName]){
+      res.json(rappers[rapName])  
+    }else{
+      res.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`)
+})
